5c985f31a47f930024dc94a8ea3fa2743dec23a4
amifistore <storeamifi@gmail.com>
2025-10-07 18:05:51 +0700

    Update broadcast_handler.py

--- broadcast_handler.py
+++ broadcast_handler.py
@@ -1,15 +1,12 @@
+import config
 from telegram.ext import CommandHandler, CallbackContext
 from telegram import Update, Bot
 import database
-import os
 
-BOT_TOKEN = os.environ.get("TOKEN_BOT_KAMU")
-bot = Bot(BOT_TOKEN)
+bot = Bot(config.BOT_TOKEN)
 
 def broadcast(update: Update, context: CallbackContext):
-    user = update.message.from_user
-    user_id = database.get_or_create_user(str(user.id), user.username, user.full_name)
-    if not database.is_admin(user_id):
+    if str(update.message.from_user.id) not in config.ADMIN_TELEGRAM_IDS:
         update.message.reply_text("Hanya admin yang bisa broadcast.")
         return
     text = " ".join(context.args)
@@ -19,7 +16,7 @@ def broadcast(update: Update, context: CallbackContext):
     for telegram_id in database.get_all_telegram_ids():
         try:
             bot.send_message(chat_id=telegram_id, text=f"[INFO]: {text}")
-        except Exception as e:
+        except Exception:
             pass
     update.message.reply_text("Pesan broadcast sudah dikirim ke semua user.")
 
