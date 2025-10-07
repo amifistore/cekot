5234d95149a421c23cb2fe66d188c097d9b568b2
amifistore <storeamifi@gmail.com>
2025-10-07 16:20:42 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,16 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Application, CommandHandler
 from topup_handler import topup_conv_handler
 from order_handler import order_conv_handler
 import database
 
 def main():
     database.init_db()
-    updater = Updater("7976276575:AAE8-jSX-T5KlYDTbsYKGn1K3xd25WtY39Y", use_context=True)
-    dp = updater.dispatcher
+    application = Application.builder().token("TOKEN_BOT_KAMU").build()
 
-    dp.add_handler(topup_conv_handler)
-    dp.add_handler(order_conv_handler)
+    application.add_handler(topup_conv_handler)
+    application.add_handler(order_conv_handler)
 
-    updater.start_polling()
-    updater.idle()
+    application.run_polling()
 
 if __name__ == "__main__":
     main()
